bdf1473fb61ffdcde69d3fd5debea4ef676f9d49
Magdalena Badura <magdexxa@gmail.com>
2020-05-16 23:36:06 +0200

    add start and end screens

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ class Game:
         self.running = True
         self.world_shift = 0
         self.can_jump = True
+        self.font_name = pg.font.match_font(FONT_NAME)
 
     def create_wall(self, x, y, n):
         for i in range(n):
@@ -97,14 +98,13 @@ class Game:
         for enemy in self.enemy_list:
             if abs(enemy.rect.x - self.player.rect.x) == self.player.rect.width / 2 and self.player.rect.bottom == enemy.rect.bottom:
                 self.player.kill()
+                self.playing = False
 
         self.all_sprites.update()
         self.worm.update()
 
     def events(self):
         # Game Loop - events
-
-        go_right = False
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -141,8 +141,8 @@ class Game:
 
 
         if( abs( (self.right_wall.rect.x - self.right_wall.rect.width / 2) - (self.player.rect.x + self.player.rect.width / 2)) == self.player.rect.width):
+            self.go_to_store()
             self.playing = False
-            self.running = False
 
     
     def draw(self):
@@ -153,10 +153,22 @@ class Game:
         pg.display.flip()
 
     def show_start_screen(self):
-        pass  # here will be game start screen
+        print("Start")
+        self.screen.fill(RED)
+        self.draw_text(TITLE, 48, WHITE, WIDTH / 2, HEIGHT / 4)
+        self.draw_text("Use arrows to move and jump", 22, WHITE, WIDTH / 2, HEIGHT / 2)
+        self.draw_text("Press a key to play", 22, WHITE, WIDTH / 2, HEIGHT * 3 / 4)
+        pg.display.flip()
+        self.wait_for_key()
 
     def show_go_screen(self):
-        pass  # here will be end game screen
+        print("End")
+        if not self.running:
+            return
+        self.screen.fill(RED)
+        self.draw_text("GAME OVER", 48, WHITE, WIDTH / 2, HEIGHT / 4)
+        pg.display.flip()
+        self.wait_for_key()
 
     def shift_world(self, shift_x):
         self.world_shift += shift_x
@@ -169,6 +181,29 @@ class Game:
 
         for enemy in self.enemy_list:
             enemy.rect.x += shift_x
+
+
+    def wait_for_key(self):
+        waiting = True
+        while waiting:
+            self.clock.tick(FPS)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    waiting = False
+                    self.running = False
+                if event.type == pg.KEYUP:
+                    waiting = False
+
+    def draw_text(self, text, size, color, x, y):
+        font = pg.font.Font(self.font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x, y)
+        self.screen.blit(text_surface, text_rect)
+
+    def go_to_store(self):
+        print("is in store")
+        self.show_go_screen()
 
 g = Game()
 g.show_start_screen()
