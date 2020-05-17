fb1eca2a5014d241811304e9805288761b134753
Magdalena Badura <magdexxa@gmail.com>
2020-05-17 15:00:21 +0200

    add gold

--- sprites.py
+++ sprites.py
@@ -94,3 +94,14 @@ class Platform(pg.sprite.Sprite):
         self.rect.y = y
         self.width = w
         self.height = h
+        
+class Gold(pg.sprite.Sprite):
+    def __init__(self, x, y, w, h):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((w, h))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.width = w
+        self.height = h
