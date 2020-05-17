b69444e8b2b3e31822bf83188e25104c366098d2
szelemeh <subzarro6@gmail.com>
2020-05-17 20:06:33 +0300

    Changed enemies

--- sprites/enemies/air_enemies/air_enemy.py
+++ sprites/enemies/air_enemies/air_enemy.py
@@ -2,8 +2,8 @@ from sprites.enemies.enemy import *
 
 
 class AirEnemy(Enemy):
-    def __init__(self, x, y, width, height, target):
-        super().__init__(x, y, width, height, target)
+    def __init__(self, x, y, width, height, images_left, images_right, target):
+        super().__init__(x, y, width, height, images_left, images_right, target)
 
     def update(self):
 
--- sprites/enemies/air_enemies/ghost.py
+++ sprites/enemies/air_enemies/ghost.py
@@ -2,10 +2,8 @@ from sprites.enemies.air_enemies.air_enemy import AirEnemy, Enemy
 
 
 class Ghost(AirEnemy):
-    def __init__(self, x, y, width, height, target):
-        super().__init__(x, y, width, height, target)
-
-        Enemy.load_images(self, '../../../img/ghost/ghost.png')
+    def __init__(self, x, y, width, height, images_left, images_right, target):
+        super().__init__(x, y, width, height, images_left, images_right, target)
 
     def update(self):
 
