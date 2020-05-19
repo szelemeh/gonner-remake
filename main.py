e547f204b58178348aac4c4bf63dc049b6f8e8aa
Magdalena Badura <magdexxa@gmail.com>
2020-05-19 23:17:39 +0200

    Add multiple levels

--- main.py
+++ main.py
@@ -24,21 +24,28 @@ class Game:
 
         self.navigator = Navigator()
 
-    def new(self):
         self.creator = Creator()
         self.creator.create_gold(WIDTH / 2, HEIGHT / 2)
 
-        self.gold = pg.sprite.Group()
-
         self.player = self.creator.create_player(WIDTH / 3, HEIGHT / 2)
         self.player.weapon = Weapon(self.creator)
+
+        self.all_sprites = self.creator.all_sprites
+        self.platform_list = self.creator.platforms
+        self.tile_list = self.creator.tiles
+        self.enemy_list = self.creator.enemies
+        self.coin_list = self.creator.coins
+        self.player.collide_list = self.creator.player_collide_list
+        self.camera = Camera(self.player, self.creator.shiftable)
+        self.bullet_list = self.creator.bullets
+
+
+    def build_level_01(self):
 
         for i in range(1, 5):
             self.creator.create_enemy(EnemyType.WORM, WIDTH / 3, HEIGHT / 2, self.player)
             self.creator.create_enemy(EnemyType.SLIME, WIDTH - 50, HEIGHT - 50, self.player)
             self.creator.create_enemy(EnemyType.GHOST, WIDTH - 50, HEIGHT / 2, self.player)
-
-        # self.creator.create_enemy(EnemyType.SLIME_BLOCK, -500, HEIGHT / 2, self.player)
 
         self.right_wall = self.creator.create_platform(WIDTH * 4, 0, 120, HEIGHT)
 
@@ -62,21 +69,44 @@ class Game:
             n = randint(1, 4)
             self.creator.create_wall(room[3][random][0], room[3][random][1], n)
 
-        self.all_sprites = self.creator.all_sprites
-        self.platform_list = self.creator.platforms
-        self.tile_list = self.creator.tiles
-        self.enemy_list = self.creator.enemies
-        self.coin_list = self.creator.coins
-        self.player.collide_list = self.creator.player_collide_list
-        self.camera = Camera(self.player, self.creator.shiftable)
-        self.bullet_list = self.creator.bullets
-        self.run()
 
-    def run(self):
+
+    def build_level_02(self):
+
+        self.creator.empty_all_objects()
+        self.all_sprites.add(self.player)
+
+        self.right_wall = self.creator.create_platform(WIDTH * 4, 0, 120, HEIGHT)
+
+
+    def build_level_03(self):
+
+        self.creator.empty_all_objects()
+        self.all_sprites.add(self.player)
+
+        self.right_wall = self.creator.create_platform(WIDTH * 4, 0, 120, HEIGHT)
+
+        self.creator.create_enemy(EnemyType.SLIME_BLOCK, WIDTH / 2, HEIGHT / 2, self.player)
+
+
+    def new(self, number):
+        if(number == 0):
+            self.build_level_01()
+            self.run(number)
+        elif(number == 1):
+            self.build_level_02()
+            self.run(number)
+        elif(number == 2):
+            self.build_level_03()
+            self.run(number)
+        else:
+            return
+
+    def run(self, number):
         self.playing = True
         while self.playing:
             self.clock.tick(FPS)
-            self.events()
+            self.events(number)
             self.update()
             self.draw()
 
@@ -107,13 +137,15 @@ class Game:
         self.all_sprites.update()
         self.camera.update()
 
-    def events(self):
+    def events(self, number):
         have_jumped = False
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 if self.playing:
                     self.playing = False
                 self.running = False
+                self.navigator.show_go_screen()
+                pg.quit()
 
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_SPACE:
@@ -148,7 +180,10 @@ class Game:
 
         if abs((self.right_wall.rect.x - self.right_wall.rect.width / 2) - (
                 self.player.rect.x + self.player.rect.width / 2)) <= 25:
-            self.navigator.go_to_store()
+                if(number == 0 or number == 1):
+                    self.navigator.go_to_store()
+                else:
+                    self.navigator.show_go_screen()
 
     def draw_stats_bar(self):
         stats = "HP: " + str(self.player.hp) + ", money: " + str(self.player.money)
@@ -165,12 +200,16 @@ class Game:
         pg.display.flip()
 
 
+
+Levels = 3
 g = Game()
 nav = Navigator()
 nav.set_game(g)
 nav.show_start_screen()
 while g.running:
-    g.new()
-    nav.show_go_screen()
-
+    for lvl in range(Levels):
+        print("You're on ", lvl)
+        g.new(lvl)
+    g.running = False
+nav.show_go_screen()
 pg.quit()
